bd11be758eda20c9746206f9348e702a3d34e4b9
jdichh <jpsdichoso21@gmail.com>
2023-10-15 20:22:14 +0800

    style: changed icon colors, changed logo

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,28 +1,40 @@
-import { LayoutDashboard } from "lucide-react";
+import { Code2Icon, HeadphonesIcon, ImageIcon, MessagesSquare, SettingsIcon, VideoIcon } from "lucide-react";
 
 export const links = [
     {
-        title: "Dashboard",
-        icon: LayoutDashboard,
-        href: "/dashboard",
+        title: "Converse",
+        icon: MessagesSquare,
+        href: "/converse",
+        color: "text-emerald-500"
+    },
+    {
+        title: "Image Generation",
+        icon: ImageIcon,
+        href: "/image-generation",
+        color: "text-amber-500"
+    },
+    {
+        title: "Video Generation",
+        icon: VideoIcon,
+        href: "/vide-generation",
+        color: "text-lime-500"
+    },
+    {
+        title: "Audio Generation",
+        icon: HeadphonesIcon,
+        href: "/audio-generation",
+        color: "text-teal-500"
+    },
+    {
+        title: "Code Generation",
+        icon: Code2Icon,
+        href: "/code-generation",
         color: "text-sky-500"
     },
     {
-        title: "Dashboard",
-        icon: LayoutDashboard,
-        href: "/dashboard",
-        color: "text-sky-500"
-    },
-    {
-        title: "Dashboard",
-        icon: LayoutDashboard,
-        href: "/dashboard",
-        color: "text-sky-500"
-    },
-    {
-        title: "Dashboard",
-        icon: LayoutDashboard,
-        href: "/dashboard",
-        color: "text-sky-500"
+        title: "Settings",
+        icon: SettingsIcon,
+        href: "/settings",
+        color: "text-slate-100"
     },
 ]

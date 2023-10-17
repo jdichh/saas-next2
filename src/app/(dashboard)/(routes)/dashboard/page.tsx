1a75994806ae67af9086cbc269311e94918bfc33
jdichh <jpsdichoso21@gmail.com>
2023-10-17 11:10:29 +0800

    refactor: removed redundant array

--- src/app/(dashboard)/(routes)/dashboard/page.tsx
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Card } from "@/components/ui/card";
-import { tools } from "@/lib/data";
+import { links } from "@/lib/data";
 import { cn } from "@/lib/utils";
 import { ArrowRight } from "lucide-react";
 import { useRouter } from "next/navigation";
@@ -19,17 +19,17 @@ export default function Page() {
         </h3>
       </hgroup>
       <div className="px-2 space-y-2 w-full max-w-4xl mx-auto">
-        {tools.map((tool) => (
+        {links.map((link) => (
           <Card
-            onClick={() => router.push(tool.href)}
-            key={tool.href}
+            onClick={() => router.push(link.href)}
+            key={link.href}
             className="flex items-center justify-between p-4 border-black/5 transition cursor-pointer hover:scale-[1.015] hover:shadow-md active:scale-[0.985]"
           >
             <div className="flex items-center gap-x-2 ">
-              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
-                <tool.icon className={cn("w-5 h-5", tool.color)} />
+              <div className={cn("p-2 w-fit rounded-md", link.bgColor)}>
+                <link.icon className={cn("w-5 h-5", link.color)} />
               </div>
-              <span className="font-medium">{tool.title}</span>
+              <span className="font-medium">{link.title}</span>
             </div>
             <ArrowRight className="w-5 h-5 transition" />
           </Card>
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -3,55 +3,10 @@ import {
   HeadphonesIcon,
   ImageIcon,
   MessagesSquare,
-  SettingsIcon,
   VideoIcon,
 } from "lucide-react";
 
 export const links = [
-  {
-    title: "Converse",
-    icon: MessagesSquare,
-    href: "/converse",
-    bgColor: "bg-emerald-50",
-    color: "text-emerald-600",
-  },
-  {
-    title: "Image Generation",
-    icon: ImageIcon,
-    href: "/image-generation",
-    bgColor: "bg-amber-50",
-    color: "text-amber-600",
-  },
-  {
-    title: "Video Generation",
-    icon: VideoIcon,
-    href: "/video-generation",
-    bgColor: "bg-lime-50",
-    color: "text-lime-600",
-  },
-  {
-    title: "Audio Generation",
-    icon: HeadphonesIcon,
-    href: "/audio-generation",
-    bgColor: "bg-teal-50",
-    color: "text-teal-600",
-  },
-  {
-    title: "Code Generation",
-    icon: Code2Icon,
-    href: "/code-generation",
-    bgColor: "bg-sky-50",
-    color: "text-sky-600",
-  },
-  {
-    title: "Settings",
-    icon: SettingsIcon,
-    href: "/settings",
-    color: "text-slate-50",
-  },
-];
-
-export const tools = [
   {
     title: "Converse",
     icon: MessagesSquare,

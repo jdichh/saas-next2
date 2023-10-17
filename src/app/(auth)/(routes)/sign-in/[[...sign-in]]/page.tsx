432de5a0403d674870b2d645bf2911a9ff240848
jdichh <jpsdichoso21@gmail.com>
2023-10-17 14:02:31 +0800

    added demo account

--- src/app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,13 +1,22 @@
 import { SignIn } from "@clerk/nextjs";
 import ClerkNavbar from "../../clerk-navbar";
- 
+
 export default function Page() {
   return (
     <div className="h-full w-full">
       <ClerkNavbar />
-      <main className="flex items-center justify-center h-full">
+      <main className="flex flex-col items-center justify-center h-full">
         <SignIn />
+        <div className="font-sm text-center text-muted-foreground mt-8">
+          <p>
+            <span className="font-semibold">Gmail Address: </span>
+            dreamscapedemo@gmail.com
+          </p>
+          <p>
+            <span className="font-semibold">Password: </span>Dreamscape01
+          </p>
+        </div>
       </main>
     </div>
-  )
+  );
 }
